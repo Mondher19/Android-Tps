332a7992b2e76b12883c445b45055812f4ba9c26
Mondher19 <mondher.benhajammar@esprit.tn>
2023-02-10 12:05:18 +0100

    first commit

---
+++ app/src/main/java/com/example/colormixer/FailureActivity.kt
@@ -0,0 +1,11 @@
+package com.example.colormixer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FailureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_failure)
+    }
+}
