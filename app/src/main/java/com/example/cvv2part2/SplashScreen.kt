00f9268688569af43e43e01e0c2e7f9ad50678d5
Mondher19 <mondher.benhajammar@esprit.tn>
2023-03-02 19:07:40 +0100

    commit message

---
+++ app/src/main/java/com/example/cvv2part2/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.cvv2part2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cvv2part2.MainActivity
+import java.util.logging.Handler
+
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT = 3000L // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
---
+++ app/src/main/java/com/example/cvv2part2/secondActivity.kt
@@ -0,0 +1,84 @@
+package com.example.cvv2part2
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+
+import com.example.cvv2part2.R
+import com.example.cvv2part2.Skills
+import com.example.cvv2part2.Hobbies
+import com.example.cvv2part2.Languages
+
+
+class secondActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val fullname = intent.getStringExtra("fullname")
+        val email = intent.getStringExtra("email")
+        val age = intent.getStringExtra("age")
+        val byteArray = intent.getByteArrayExtra("profileimage")
+        val bitmap = byteArray?.let { BitmapFactory.decodeByteArray(byteArray, 0, it.size) }
+        val profileimage = findViewById<ImageView>(R.id.profileimage)
+        profileimage.setImageBitmap(bitmap)
+
+        findViewById<TextView>(R.id.fullname).text = fullname
+        findViewById<TextView>(R.id.email).text = email
+
+
+
+        val skillsbtn = findViewById<Button>(R.id.skillsbtn)
+        skillsbtn.setOnClickListener {
+            val fragment = Skills()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentSecondAct, fragment).commit()
+        }
+
+        val hobbiesbtn = findViewById<Button>(R.id.hobbiesbtn)
+        hobbiesbtn.setOnClickListener {
+            val fragment = Hobbies()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentSecondAct, fragment).commit()
+        }
+
+        val languagesbtn = findViewById<Button>(R.id.languagesbtn)
+        languagesbtn.setOnClickListener {
+            val fragment = Languages()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentSecondAct, fragment).commit()
+        }
+
+
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, android.R.color.white))
+        supportActionBar?.apply {
+            title = "Create your resume"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+        // Get a reference to the ImageView in your layout
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
+        return true
+    }
+
+
+
+
+}
